bcccf95568d0ab0eaebd3095b8dcc7ea7ac5bcfe
vigneshwaran0 <66939485+vigneshwaran0@users.noreply.github.com>
2021-02-15 23:11:38 +0530

    Create Page.php

---
+++ Page.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home || Kratigence</title>
+        <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+                    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+                        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+                            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+                                    <style>
+                                            .navbar-brand img {
+                                                        border-radius: 50%;
+                                                                    position: topleft;
+                                                                                width: 45px;
+                                                                                            height: 45px;
+                                                                                                        margin-top: -13px;
+                                                                                                                    margin-left: -80px;
+                                                                                                                            }
+        .navbar {
+                    margin-bottom: 0;
+                                background-color: black;
+                                            z-index: 9999;
+                                                        border: 0;
+                                                                    font-size: 20px !important;
+                                                                                line-height: 1.42857143 !important;
+                                                                                            border-radius: 0;
+        }
+        #navbar-1 {
+                    margin-bottom: 0;
+                                background-color: rgb(216, 209, 209);
+                                            z-index: 9999;
+                                                        border: 0;
+                                                                    font-size: 20px !important;
+                                                                                line-height: 1.42857143 !important;
+                                                                                            border-radius: 0;
+        }
+        .nav.navbar-nav.navbar-right li a {
+                    color: white;
+                            }
+        .navbar-nav li a:hover,
+                .navbar-nav li.active a {
+                            color: #6d6765 !important;
+                                        background-color: #fff !important;
+                                                }
+        .jumbotron {
+                    background-image: url("bg.jpg");
+                                color: black;
+                                            padding: 100px 25px;
+                                                        font-family: Montserrat, sans-serif;
+                                                                }
+        .jumbotron h1 {
+                    color: blue;
+                                font-family: 'Courier New', Courier, monospace;
+                                        }
+        .jumbotron h1 span {
+                    color: red;
+                            }
+        .jumbotron p {
+                    color: black;
+                                font-family: cursive;
+                                        }
+        .jumbotron a {
+                    color: black;
+                            }
+        #about {
+                    text-align: center;
+                                background-image: linear-gradient(to right, #FFCC00, #FFFFCC);
+                                            padding: 60px 100px;
+                                                    }
+        .container-fluid h2 {
+                    color: black;
+                                font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+                                        }
+        .container-fluid h2 span {
+                    color: black;
+                                font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+                                        }
+        .container-fluid-1 {
+                    background-color: white;
+                                padding: 60px 60px;
+                                        }
+        .col-sm-8 p {
+                    background-color: rgba(26, 78, 247, 0.596);
+                                padding: 50px 50px;
+                                        }
+        .col-sm-4 h2 {
+                    color: red;
+                                text-align: center;
+                                            padding-top: 50px;
+                                                        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+                                                                }
+        .container-1 {
+                    margin: 60px;
+                                background-color: rgb(216, 209, 209);
+                                        }
+        .container-2 h2 {
+                    margin: 60px;
+                                font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+                                        }
+        .text-block {
+                    position: absolute;
+                                color: rgb(216, 209, 209);
+                                            padding-left: 20px;
+                                                        padding-right: 20px;
+                                                                }
+        .text-block h1 {
+                    color: black;
+                            }
+        #row2 .col-sm-4 {
+                    height: 200px;
+                                width: 400px;
+        }
+        .row .col-sm-4 h4 {
+                    margin-left: 40px;
+                                margin-top: 30px;
+                                        }
+        .row .col-sm-4 p {
+                    text-align: left;
+                                margin-left: 40px;
+                                            margin-top: 20px;
+                                                    }
+        #progress1 {
+                    margin: 60px;
+                                width: 100px;
+                                            height: 10px;
+        }
+        #progress5 {
+                    margin: 60px;
+                                width: 100px;
+                                            height: 10px;
+        }
+        #progress2 {
+                    margin: 60px;
+                                width: 100px;
+                                            height: 10px;
+        }
+        #progress3 {
+                    margin: 60px;
+                                width: 100px;
+                                            height: 10px;
+        }
+        #progress4 {
+                    margin: 60px;
+                                width: 100px;
+                                            height: 10px;
+        }
+        #progress {
+                    width: 100%;
+                                height: 10px;
+                                        }
+        mark {
+                    margin: 60px;
+                                background-color: rgba(230, 134, 105, 0.808);
+                                        }
+        .container-3 {
+                    text-align: center;
+                                color: white;
+                                            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+                                                        background-image: linear-gradient(to right, rgb(31, 65, 45), rgb(45, 139, 84), rgb(31, 65, 45));
+                                                                }
+        .container-3 .row {
+                    background-color: white;
+                                color: black;
+                                        }
+        .footer {
+                    background-color: black;
+                                color: white;
+                                        }
+        .social li {
+            border: 2px solid #B5B5B5;
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            -o-border-radius: 50%;
+            -ms-border-radius: 50%;
+            border-radius: 50%;
+            float: left;
+            height: 36px;
+            line-height: 36px;
+            margin: 0 8px 0 0;
+            padding: 0;
+            text-align: center;
+            width: 36px;
+            transition: all 0.5s ease 0s;
+            -moz-transition: all 0.5s ease 0s;
+            -webkit-transition: all 0.5s ease 0s;
+            -ms-transition: all 0.5s ease 0s;
+            -o-transition: all 0.5s ease 0s;
+        }
+
+
+        .social li a {
+            color: #EDEFF1;
+        }
+        .social li:hover {
+            border: 2px solid #b8c9da;
+            background: #a6b8ca;
+        }
+        .social li a i {
+            font-size: 16px;
+            margin: 0 0 0 5px;
+            color: #EDEFF1 !important;
+        }
+            </style>
+            </head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+                                                                                                                                    <a class="navbar-brand" href="#myPage"><img src="logo.jpeg" alt="logo"></img></a>
+                                                                                                                                                </div>
+                                                                                                                                                            <div class="collapse navbar-collapse" id="myNavbar">
+                                                                                                                                                                            <ul class="nav navbar-nav navbar-right">
+                                                                                                                                                                                                <li><a href="#myPage">Home</a></li>
+                                                                                                                                                                                                                    <li><a href="#about">About Us</a></li>
+                                                                                                                                                                                                                                        <li><a href="#event">Events</a></li>
+                                                                                                                                                                                                                                                            <li><a href="#intern">Internships</a></li>
+                                                                                                                                                                                                                                                                                <li><a href="#project">Project</a></li>
+                                                                                                                                                                                                                                                                                                    <li><a href="#team">Team</a></li>
+                                                                                                                                                                                                                                                                                                                        <li><a href="#blog">Blog</a></li>
+                                                                                                                                                                                                                                                                                                                                            <li><a href="#sponser">Sponsors</a></li>
+                                                                                                                                                                                                                                                                                                                                                                <li><a href="#contact">Contact Us</a></li>
+                                                                                                                                                                                                                                                                                                                                                                                </ul>
+                                                                                                                                                                                                                                                                                                                                                                                            </div>
+                                                                                                                                                                                                                                                                                                                                                                                                    </div>
+                                                                                                                                                                                                                                                                                                                                                                                                        </nav>
+                                                                                                                                                                                                                                                                                                                                                                                                            <div class="jumbotron text-center">
+                                                                                                                                                                                                                                                                                                                                                                                                                    <h1><b>KRAti<span>GENCE</span></b></h1>
+                                                                                                                                                                                                                                                                                                                                                                                                                            <h2>IIIT BHOPAL</h2>
+                                                                                                                                                                                                                                                                                                                                                                                                                                    <p><b>WE AIM TOWARDS SOLVING REAL WORLD PROBLEMS THROUGH ARTIFICIAL INTELLIGENCE.</b></p>
+                                                                                                                                                                                                                                                                                                                                                                                                                                            <a href="#myPage" title="To Down">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        <span class="glyphicon glyphicon-chevron-down"></span>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                </a>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                    </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                        <div id="about" class="container-fluid">
+                <br>The society is open to students with or without experience because, believe it or not, none of us
+                                had any idea of AI when we first stepped into the society room,
